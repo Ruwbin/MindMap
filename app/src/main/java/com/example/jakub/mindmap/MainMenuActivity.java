4f374a6d19cb28aaf356816bc2788a39327af687
Stanisław <malpunek@gmail.com>
2015-07-21 21:55:40 +0200

    Menu obrazkowe, new odpala nowa activity(tworzenia map), jak sie kliknie i pusci poza obrazkiem to nic sie nie dzieje :)

--- app/src/main/java/com/example/jakub/mindmap/MainMenuActivity.java
+++ app/src/main/java/com/example/jakub/mindmap/MainMenuActivity.java
@@ -15,21 +15,38 @@ import android.widget.LinearLayout;
 
 public class MainMenuActivity extends ActionBarActivity {
 
-    ImageView newimg,newimgclck;
-    LinearLayout lay;
+    ImageView newimg,setimg,broimg,stoimg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         newimg = (ImageView) findViewById(R.id.imageView);
-        newimgclck = (ImageView) findViewById(R.id.imageView5);
+        stoimg = (ImageView) findViewById(R.id.imageView2);
+        broimg = (ImageView) findViewById(R.id.imageView3);
+        setimg = (ImageView) findViewById(R.id.imageView4);
+
         View.OnTouchListener kuuupa = new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                ImageView dotkniety = (ImageView) findViewById(R.id.imageView);
-                ImageView wyswietl = (ImageView) findViewById(R.id.imageView5);
+                ImageView dotkniety = (ImageView) findViewById(v.getId());
+                ImageView wyswietl;
                 switch (v.getId()) {
                     case R.id.imageView:
+                        wyswietl = (ImageView) findViewById(R.id.imageView5);
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                            dotkniety.setVisibility(View.GONE);
+                            wyswietl.setVisibility(View.VISIBLE);
+                        }
+                        else if (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_OUTSIDE){
+                            dotkniety.setVisibility(View.VISIBLE);
+                            wyswietl.setVisibility(View.GONE);
+                            if(isPointInsideView(event.getRawX(), event.getRawY(), dotkniety)){
+                                CreateNewMap();
+                            }
+                        }
+                        break;
+                    case R.id.imageView2:
+                        wyswietl = (ImageView) findViewById(R.id.imageView6);
                         if (event.getAction() == MotionEvent.ACTION_DOWN) {
                             dotkniety.setVisibility(View.GONE);
                             wyswietl.setVisibility(View.VISIBLE);
@@ -38,25 +55,61 @@ public class MainMenuActivity extends ActionBarActivity {
                             dotkniety.setVisibility(View.VISIBLE);
                             wyswietl.setVisibility(View.GONE);
                         }
-
                         break;
-                    case R.id.imageView5:
+                    case R.id.imageView3:
+                        wyswietl = (ImageView) findViewById(R.id.imageView7);
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                            dotkniety.setVisibility(View.GONE);
+                            wyswietl.setVisibility(View.VISIBLE);
+                        }
+                        else if (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_OUTSIDE){
+                            dotkniety.setVisibility(View.VISIBLE);
+                            wyswietl.setVisibility(View.GONE);
+                        }
+                        break;
+                    case R.id.imageView4:
+                        wyswietl = (ImageView) findViewById(R.id.imageView8);
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                            dotkniety.setVisibility(View.GONE);
+                            wyswietl.setVisibility(View.VISIBLE);
+                        }
+                        else if (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_OUTSIDE){
+                            dotkniety.setVisibility(View.VISIBLE);
+                            wyswietl.setVisibility(View.GONE);
+                        }
+                        break;
+                    /*case R.id.imageView5:
                         if (event.getAction() == MotionEvent.ACTION_UP) {
                             dotkniety.setVisibility(View.VISIBLE);
                             wyswietl.setVisibility(View.GONE);
                         }
 
-                        break;
+                        break;*/
                     }
 
                 return true;
                 }
         };
         newimg.setOnTouchListener(kuuupa);
-        newimgclck.setOnTouchListener(kuuupa);
+        stoimg.setOnTouchListener(kuuupa);
+        setimg.setOnTouchListener(kuuupa);
+        broimg.setOnTouchListener(kuuupa);
 
     }
+    public static boolean isPointInsideView(float x, float y, View view){
+        int location[] = new int[2];
+        view.getLocationOnScreen(location);
+        int viewX = location[0];
+        int viewY = location[1];
 
+        //point is inside view bounds
+        if(( x > viewX && x < (viewX + view.getWidth())) &&
+                ( y > viewY && y < (viewY + view.getHeight()))){
+            return true;
+        } else {
+            return false;
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -78,10 +131,12 @@ public class MainMenuActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    public void ClickedNewButton(View view) {
+    public void CreateNewMap() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+
     }
+
     public void ClickedBrowseButton(View view){
 
 
